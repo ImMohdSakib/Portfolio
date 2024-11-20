0298358139c3e03fc3650a4313b2f5ae899203fd
Mohd Sakib <143605651+ImMohdSakib@users.noreply.github.com>
2024-11-21 00:28:40 +0530

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
     {
       title: "YouTube Clone",
       main: "A YouTube clone designed for seamless video browsing and playback, mimicking the core viewing experience in a familiar layout.",
-      demoLink: "https://example.com/youtube-demo",
+      demoLink: "https://github.com/ImMohdSakib/youtube-clone",
       sourceCodeLink: "https://github.com/ImMohdSakib/youtube-clone",
     },
     {
